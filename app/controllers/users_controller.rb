679611a286c6b53f002067161e66296365f5c9a6
Tshinori Sugita <sugita@c-fo.com>
2015-04-24 01:15:46 +0900

    add friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
@@ -44,7 +43,10 @@ class UsersController < ApplicationController
   # Before actions
 
   def signed_in_user
-    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
   end
 
   def correct_user
